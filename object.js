f6f274167d71b372f7dd4f9b2d71e830fe157167
An Quach <quachan888@gmail.com>
2022-01-15 10:25:00 -0500

    Nucamp React Workshop 1 init

---
+++ object.js
@@ -0,0 +1,13 @@
+const bicycle = {
+    color: "blue",
+    electric: false,
+    start() {
+        console.log("You begin to pedal the bike.");
+    },
+};
+
+// Type these in the console
+bicycle;
+bicycle.color;
+bicycle.start;
+bicycle.start();
