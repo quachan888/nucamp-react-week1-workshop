aa07df6401ecd8b6b592d75fc124f1bcc83c2a9b
Andy Quach <quachan888@gmail.com>
2022-01-15 14:44:21 -0500

    Assignment Week1 finish

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -7,56 +7,71 @@ class Student {
 }
 
 class Bootcamp {
-    constructor(name, level, students = []) {
+    constructor(name, level, students) {
         this.name = name;
         this.level = level;
-        this.students = students;
+        this.students = new Array();
     }
-    registerStudent(studentToRegistry) {
-        if (this.isRegFunction(studentToRegistry)) {
-            console.warn(`This student email ${studentToRegistry.email} is already registered to ${this.name}!`);
+    registerStudent(studentToRegister) {
+        let isRegistered = this.students.filter((e) => e.email === studentToRegister.email).length;
+        if (isRegistered) {
+            console.warn(`This student email ${studentToRegister.email} is already registered to ${this.name}!`);
         } else {
-            this.students.push(studentToRegistry);
-            console.log(`Registering ${studentToRegistry.email} to the bootcamp ${this.name}.`);
+            this.students.push(studentToRegister);
+            console.log(`Registering ${studentToRegister.email} to the bootcamp ${this.name}.`);
         }
+        return this.students;
     }
-    isRegFunction(studentToCheck) {
-        let isReg = false;
-        for (let each of this.students) {
-            if (each.email === studentToCheck.email) isReg = true;
+
+    dropStudent(studentToDrop) {
+        let isRegistered = this.students.some((e) => e.email === studentToDrop.email);
+        if (isRegistered) {
+            this.students.splice(this.students.indexOf(studentToDrop), 1);
+            console.log(`Student ${studentToDrop.email} is removed from Bootcamp ${this.name}`);
+        } else {
+            console.warn(`Student ${studentToDrop.email} is not in ${this.name}!`);
         }
-        return isReg;
+        return this.students;
+    }
+    countStudent() {
+        let result = this.students.length;
+        console.log(`There are ${result} students in ${this.name}.`);
     }
 }
 
-// Create one or more student object using the Student class.
-console.log("=== Create one or more student object using the Student class.");
+console.log("===== TASK 1 =====");
 let s1 = new Student("Andy", "111@gmail.com", "Sandy Spring");
 let s2 = new Student("Sandy", "222@gmail.com", "San Jose");
 let s3 = new Student("Neo", "333@live.com", "Tacoma");
 console.log(s1);
 console.log(s2);
 console.log(s3);
-console.log("");
 
-// Create a new bootcamp using the Bootcamp class.
-console.log("==== Create a new bootcamp using the Bootcamp class.");
 const webDevFundamentals = new Bootcamp("Web Developement Fundamentals", "Beginner");
 const fullStack = new Bootcamp("Full Stack Web and Mobile Development", "Advanced");
 console.log(webDevFundamentals);
 console.log(fullStack);
 
-// Register the student object(s) to one of the bootcamp objects you created. Verify that registration succeeded.
 console.log("");
-console.log("=== Register the student object(s) to one of the bootcamp objects ");
+console.log("===== TASK 2: REGISTER STUDENT =====");
 webDevFundamentals.registerStudent(s1);
 webDevFundamentals.registerStudent(s2);
 fullStack.registerStudent(s1);
 fullStack.registerStudent(s3);
-
-// Verify that you cannot register the same student twice to a Bootcamp.
 webDevFundamentals.registerStudent(s2);
 fullStack.registerStudent(s1);
 
 console.log(webDevFundamentals);
 console.log(fullStack);
+
+console.log("");
+console.log("===== BONUS TASK 1: DROP STUDENT =====");
+webDevFundamentals.dropStudent(s2);
+webDevFundamentals.dropStudent(s3);
+console.log(webDevFundamentals);
+console.log(fullStack);
+
+console.log("");
+console.log("===== BONUS TASK 2: COUNT STUDENT =====");
+webDevFundamentals.countStudent();
+fullStack.countStudent();
